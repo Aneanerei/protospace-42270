a4eee9794e8cd2123c30392ab2b7b8f2cb68ac95
Aneanerei <tamu.m.thr@gmail.com>
2025-04-16 20:27:53 +0900

    バリデーション追加

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -5,4 +5,7 @@ class Prototype < ApplicationRecord
   validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true
+  validates :image, presence: true
+  private
+
 end
